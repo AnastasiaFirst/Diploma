758e007d445575daf1508065e5c820333926c881
AnastasiaFirst <92177620+AnastasiaFirst@users.noreply.github.com>
2025-05-08 14:22:21 +0300

    Update AuthorizationExceptionHandler.java

--- src/main/java/com/example/cloud_service_diploma/exception/advice/AuthorizationExceptionHandler.java
+++ src/main/java/com/example/cloud_service_diploma/exception/advice/AuthorizationExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.example.cloud_service_diploma.exception.advice;
 
 import com.example.cloud_service_diploma.exception.BadCredentials;
-import com.example.cloud_service_diploma.exception.SuccessAuthorization;
-import com.example.cloud_service_diploma.exception.SuccessLogout;
 import com.example.cloud_service_diploma.model.Error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,17 +12,4 @@ public class AuthorizationExceptionHandler {
         Error error = new Error("Bad credentials" + ex.getMessage(), 400);
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
-
-    @ExceptionHandler(SuccessAuthorization.class)
-    public ResponseEntity<Error> handleSuccessAuthorizationException(SuccessAuthorization ex) {
-        Error error = new Error("Success authorization" + ex.getMessage(), 200);
-        return new ResponseEntity<>(error, HttpStatus.OK);
-    }
-
-    @ExceptionHandler(SuccessLogout.class)
-    public ResponseEntity<Error> handleSuccessLogoutException(SuccessLogout ex) {
-        Error error = new Error("Success logout" + ex.getMessage(), 200);
-        return new ResponseEntity<>(error, HttpStatus.OK);
-    }
-
 }
