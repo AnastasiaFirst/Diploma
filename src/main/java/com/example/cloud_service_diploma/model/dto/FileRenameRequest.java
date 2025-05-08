54a0ac68a7a074d89a06336e06338c12a8a5d576
AnastasiaFirst <92177620+AnastasiaFirst@users.noreply.github.com>
2025-05-08 14:23:21 +0300

    Create FileRenameRequest.java

---
+++ src/main/java/com/example/cloud_service_diploma/model/dto/FileRenameRequest.java
@@ -0,0 +1,13 @@
+package com.example.cloud_service_diploma.model.dto;
+
+public class FileRenameRequest {
+    private String newFileName;
+
+    public String getNewFileName() {
+        return newFileName;
+    }
+
+    public void setNewFileName(String newFileName) {
+        this.newFileName = newFileName;
+    }
+}
